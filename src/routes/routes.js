be7b7e60acfc25a9dbc0187aad8b24262cb36404
xc0d312 <geovannycortez0102@gmail.com>
2022-08-19 00:21:11 -0600

    segundo commit desde rama Dev

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,7 @@ const { Router } = require("express")
 
 const  router = Router()
 router.get('/',(req,res)=>{
-    res.render('index',({title:"website nodejs"}))
+    res.render('index',({title:"nodejs"}))
 })
 router.get('/about',(req,res)=>{
     res.render('about',({title:"about me"}))
