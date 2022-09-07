30c545f9310304faa478c2f35380c6e7fdf6c674
xc0d312 <geovannycortez0102@gmail.com>
2022-09-06 22:39:32 -0600

    funcionalidad de botones y personalizacion

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -49,7 +49,7 @@ async function main(){
 }    
 async function currentVideos(current){
     const videos = await main()
-    currentContainer.innerHTML = `<iframe width="100%" height="500" src="https://www.youtube.com/embed/${videos[current].foo.id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    currentContainer.innerHTML = `<iframe width="100%" height="400" src="https://www.youtube.com/embed/${videos[current].foo.id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 }
 currentVideos(current)
 btnNext.addEventListener('click',async(e)=>{
